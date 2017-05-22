bf20782a487c9499e004f6e3d2a6d8878830f044
Martin Kersner <m.kersner@gmail.com>
2017-05-22 22:17:48 +0900

    keep information only about the closest obstacle; NAN derivate is still present

--- js/deepq.js
+++ js/deepq.js
@@ -6,7 +6,10 @@ var player = new Player();
 var clock = 0;
 
 var actionInterval = 50;
-var distSpeedThreshold = 13;
+
+function sigmoid(t) {
+  return 1/(1+Math.pow(Math.E, -t));
+}
 
 // obstacle auxiliary variables
 var obstacleDetected = false;
@@ -14,14 +17,16 @@ var obstacle_xPos;
 var obst;
 var trex_xPos;
 var feature_history = [];
-var avg = [6.175325951887915,434.9119005894536,9.353512824597738,18.91190058945356,0.6445754341245818,553.1105623705591,5.0758324040146565,10.260474749083958,0.3466624183527163]
-var std = [0.11507956470875452,214.75567671662398,10.88515624967912,21.91747480737234,0.7992795658365198,95.53521060230698,9.29158670644035,18.724881209875324,0.6664138865328865];
+//var avg = [6.175325951887915,434.9119005894536,9.353512824597738,18.91190058945356,0.6445754341245818,553.1105623705591,5.0758324040146565,10.260474749083958,0.3466624183527163]
+//var std = [0.11507956470875452,214.75567671662398,10.88515624967912,21.91747480737234,0.7992795658365198,95.53521060230698,9.29158670644035,18.724881209875324,0.6664138865328865];
+var avg = [6.175325951887915,434.9119005894536,9.353512824597738,18.91190058945356,0.6445754341245818];//,553.1105623705591,5.0758324040146565,10.260474749083958,0.3466624183527163]
+var std = [0.11507956470875452,214.75567671662398,10.88515624967912,21.91747480737234,0.7992795658365198];//,95.53521060230698,9.29158670644035,18.724881209875324,0.6664138865328865];
 
 
 // Deep Q Learning parameters
-var num_inputs = 9; // speed, obstacle distance, obstacle y-position
+var num_inputs = 5; // speed, obstacle distance, obstacle y-position
 var num_actions = 2; // JUMP, DUCK, IDLE
-var temporal_window = 1; // amount of temporal memory. 0 = agent lives in-the-moment :)
+var temporal_window = 3; // amount of temporal memory. 0 = agent lives in-the-moment :)
 var network_size = num_inputs*temporal_window + num_actions*temporal_window + num_inputs;
 
 // the value function network computes a value of taking any of the possible actions
@@ -30,18 +35,19 @@ var network_size = num_inputs*temporal_window + num_actions*temporal_window + nu
 // to just insert simple relu hidden layers.
 var layer_defs = [];
 layer_defs.push({type:'input', out_sx:1, out_sy:1, out_depth:network_size});
-layer_defs.push({type:'fc', num_neurons: 10, activation:'relu'});
-//layer_defs.push({type:'fc', num_neurons: 50, activation:'relu'});
+layer_defs.push({type:'fc', num_neurons: 50, activation:'relu'});
+layer_defs.push({type:'fc', num_neurons: 50, activation:'relu'});
 layer_defs.push({type:'regression', num_neurons:num_actions});
 
 // options for the Temporal Difference learner that trains the above net
 // by backpropping the temporal difference learning rule.
-var tdtrainer_options = {learning_rate:0.001, momentum:0.0, batch_size:64, l2_decay:0.01};
+var tdtrainer_options = {learning_rate:0.01, momentum:0.9, batch_size:64, l2_decay:0.01};
 
 var opt = {};
 opt.temporal_window = temporal_window;
 opt.experience_size = 30000;
 opt.start_learn_threshold = 1000;
+//opt.start_learn_threshold = 10;
 opt.gamma = 0.7;
 opt.learning_steps_total = 200000;
 opt.learning_steps_burnin = 3000;
@@ -73,7 +79,8 @@ setInterval(function()
 
       gameStatus = [0,
                     game.dimensions["WIDTH"], 0, 0, 0, // the first obstacle
-                    game.dimensions["WIDTH"], 0, 0, 0]; // the second obstacle
+      ];
+                    //game.dimensions["WIDTH"], 0, 0, 0]; // the second obstacle
       gameStatus[0] = currentSpeed;
 
       // NO OBSTACLES
@@ -81,7 +88,8 @@ setInterval(function()
         obstacleDetected = false;
       }
       // APPROACHING THE FIRST OBSTACLE
-      else if (game.horizon.obstacles.length == 1) {
+      //else if (game.horizon.obstacles.length == 1) {
+      else {
         obst = game.horizon.obstacles[0];
         trex_xPos = game.tRex.xPos;
 
@@ -95,30 +103,31 @@ setInterval(function()
         }
       }
       // APPROACHING THE SECOND OBSTACLE
-      else if (game.horizon.obstacles.length >= 2) {
-        obst0 = game.horizon.obstacles[0];
-        obst1 = game.horizon.obstacles[1];
-        trex_xPos = game.tRex.xPos;
+      //else if (game.horizon.obstacles.length >= 2) {
+        //obst0 = game.horizon.obstacles[0];
+        //obst1 = game.horizon.obstacles[1];
+        //trex_xPos = game.tRex.xPos;
 
-        gameStatus[1] = obst0.xPos - trex_xPos;
-        gameStatus[2] = obst0.typeConfig.width;
-        gameStatus[3] = obst0.typeConfig.height;
-        gameStatus[4] = getObstacleType(obst0);
+        //gameStatus[1] = obst0.xPos - trex_xPos;
+        //gameStatus[2] = obst0.typeConfig.width;
+        //gameStatus[3] = obst0.typeConfig.height;
+        //gameStatus[4] = getObstacleType(obst0);
 
-        gameStatus[5] = obst1.xPos - trex_xPos;
-        gameStatus[6] = obst1.typeConfig.width;
-        gameStatus[7] = obst1.typeConfig.height;
-        gameStatus[8] = getObstacleType(obst1);
+        //gameStatus[5] = obst1.xPos - trex_xPos;
+        //gameStatus[6] = obst1.typeConfig.width;
+        //gameStatus[7] = obst1.typeConfig.height;
+        //gameStatus[8] = getObstacleType(obst1);
 
-        if (trex_xPos < obst0.xPos || trex_xPos < obst1.xPos) {
-          obstacleDetected = true;
-        }
-      }
+        //if (trex_xPos < obst0.xPos || trex_xPos < obst1.xPos) {
+          //obstacleDetected = true;
+        //}
+      //}
 
       //feature_history.push(gameStatus);
       var gameStatus_avg = gameStatus.map(function(n, i) { return n - avg[i]; });
       var gameStatus_norm = gameStatus_avg.map(function(n, i) { return n / std[i]; })
-      var action = brain.forward(gameStatus_norm);
+      var gameStatus_sigm = gameStatus_norm.map(sigmoid);
+      var action = brain.forward(gameStatus_sigm);
 
       if (action == 0)
         player.do(Player.actions.JUMP);
@@ -131,22 +140,21 @@ setInterval(function()
           var obst0 = game.horizon.obstacles[0];
           var tRex_xPos = game.tRex.xPos;
 
-          if (tRex_xPos > obst0.xPos) {
-            console.log("positive reward 1");
-            brain.backward(0.5);
+          if (tRex_xPos >= obst0.xPos) {
+            //console.log("positive reward 1");
+            brain.backward(5);
+            //brain.backward(50);
           }
 
-          if (tRex_xPos > (obst0.xPos + obst0.width)) {
-            console.log("positive reward 2");
-            brain.backward(1.0);
+          if (tRex_xPos >= (obst0.xPos + obst0.width)) {
+            //console.log("positive reward 2");
+            brain.backward(10);
+            //brain.backward(50);
           }
       }
-      //else {
-        //brain.backward(game.distanceRan*0.00025);
-      //}
     }
     else { // DINO DIED
-      console.log("negative reward");
+      //console.log("negative reward");
       //if (game.horizon.obstacles[0].xPos;
       brain.backward(-1.0);
       game.restart();
